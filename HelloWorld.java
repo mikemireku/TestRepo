1d16a32b0b3a90dc3dbf7a1b3f94697a2fbfe293
mikemireku <47335247+mikemireku@users.noreply.github.com>
2019-02-04 14:29:49 -0700

    Update HelloWorld.java
    Second commit for new documentation

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,18 +1,3 @@
-/* import static org.junit.Assert.*;
-
-import java.io.PrintStream;
-import java.util.Scanner;
-
-import org.junit.Test;
-
-public class HelloWorld {
-	public static void main(String[] args) {
-	System.out.println("Hello World");
-	}
-} */
-
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 public class HelloWorld {
 	
