2bb2b2bc699488ce8c3d3c7386d1f32ad548cf13
mikemireku <47335247+mikemireku@users.noreply.github.com>
2023-08-31 17:16:12 -0500

    Add files via upload

---
+++ MikeProj01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    static int accn_g = 32;
+    double time;
+    cout << "Enter the time (seconds): ";
+    cin >> time;
+
+    cout << "Enter the time (seconds): " << time << endl;
+    cout << "The distance through which it falls is: " << (accn_g * time * time) / 2 << endl;
+    return 0;
+}
